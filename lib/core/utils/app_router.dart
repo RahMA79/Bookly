12a59e4b9d49ab00339f334a7c09d43fdca8dab2
RahMA79 <rahoma772004@gmail.com>
2025-01-18 18:55:09 +0200

    search view done

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,23 +1,28 @@
 import 'package:bookly_app/features/Home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/Home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/Splash/presentation/splash_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static final routers = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const SplashView(),
-    ),
-    GoRoute(
-      path: '/homeView',
-      builder: (context, state) => const HomeView(),
-    ),
-    GoRoute(
-      path: '/bookDetailsView',
-      builder: (context, state) => const BookDetailsView(),
-    ),
-  ],
-);
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: '/homeView',
+        builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: '/bookDetailsView',
+        builder: (context, state) => const BookDetailsView(),
+      ),
+      GoRoute(
+        path: '/searchView',
+        builder: (context, state) => const SearchView(),
+      ),
+    ],
+  );
 }
